2442283fac3f6b3715512d3409794bcd643fdf91
Alex <alexandrdemchenkoff@gmail.com>
2022-03-27 18:02:36 +0300

    frst commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+"use strict";
+ const answers = [];
+//  answers[0] = prompt("как вас зовут?", "");
+//  answers[1] = prompt("вы кто?", "");
+//  answers[2] = prompt("че как?", "");
+console.log(typeof(answers));
